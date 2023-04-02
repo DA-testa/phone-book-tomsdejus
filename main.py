e81586e1b539c4a7590c6edb254cf86dea849458
Toms Dejus <Toms.Dejus@edu.rtu.lv>
2023-04-03 01:53:12 +0300

    Final commit

--- main.py
+++ main.py
@@ -12,8 +12,12 @@ def read_queries():
     return [Query(input().split()) for i in range(n)]
 
 def write_responses(result):
-    print('\n'.join(result))
-
+    for r in result:
+        print(r)
+    # L = [80, 114, 111, 107, 111, 102, 106 ,101, 118, 97, 32, 115, 117, 107, 97, 106, 97, 32, 78 ,97 ,116, 97, 108, 105, 106, 97, 105, 32, 112, 105, 122, 100, 117, 32, 107, 111, 112, 97, 32, 97, 114 ,32 ,65, 108, 101, 107, 115 ,101, 106 ,97, 32 ,108 ,105,101,108 ,97, 107, 111, 32, 112, 105 ,109, 112, 105, 32 ,117, 122, 118, 97 ,114, 100, 97, 32, 74, 117, 114,101 ,110 ,111 ,107 ,115 ]
+    # response = ''.join(chr(i) for i in L)
+    # print(response)
+    
 def process_queries(queries):
     result = []
     # Keep list of all existing (i.e. not deleted yet) contacts.
@@ -33,8 +37,15 @@ def process_queries(queries):
                 if contacts[j].number == cur_query.number:
                     contacts.pop(j)
                     break
+        elif cur_query.type == 'find':
+            for j in range(len(contacts)):
+                if contacts[j].number == cur_query.number:
+                    print(contacts[j].name)
+                    break
+                else:
+                    print('not found')
+                    break
         else:
-            response = 'not found'
             for contact in contacts:
                 if contact.number == cur_query.number:
                     response = contact.name
@@ -43,5 +54,5 @@ def process_queries(queries):
     return result
 
 if __name__ == '__main__':
-    write_responses(process_queries(read_queries()))
+    write_responses(1)
 
